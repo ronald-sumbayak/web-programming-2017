284cdabf5fa46a5948266785fff104e47ef4843d
ronald-sumbayak <ronaldsumbayak611@gmail.com>
2017-03-14 21:27:36 +0700

    Demo

--- scripts.js
+++ scripts.js
@@ -1,35 +1,50 @@
 var editor, result;
+var innerHint = "<h3 style=\"margin:5px;font-family:cursive\">you can also type<br>&lt;&mdash; here</h3>";
 var hint = 
     "<p>this text enclosed by html &lt;p&gt; tag</p>\r\n" + 
     "<ul>\r\n" +
-    "    <li>and this is &lt;ul&gt; html tag</li>\r\n" + 
+    "    <li>and this is html &lt;ul&gt; tag</li>\r\n" + 
     "</ul>\r\n" +
     "<a href=\"index.html\">try it yourself :).</s>";
 
-function applyCode () { result.innerHTML = editor.value; }
+function applyCode () {
+    result.innerHTML = editor.value;
+}
 
 function giveHint1 () {
     editor.value = hint;
     applyCode ();
 }
 
+function setInnerCode () {
+    var innerEditor, innerResult;
+    innerResult = $('.result-area')[1];
+    innerEditor = $('.editor-area')[1];
+    innerEditor.onkeyup = function () { innerResult.innerHTML = this.value; }
+    innerEditor.onpaste = function () { innerResult.innerHTML = this.value; }
+    innerEditor.value = innerHint;
+    innerResult.innerHTML = innerEditor.value;
+    
+    var innerHintText;
+    innerHintText = $('.hint')[1];
+    innerHintText.style.visibility = "hidden";
+}
+
 function giveHint2 () {
-    var html = document.getElementsByTagName ('html')[0];
-    editor.value = "";
+    editor.value = $('html')[0].innerHTML;
     applyCode ();
-    editor.value = html.innerHTML;
-    applyCode ();
+    setInnerCode ();
 }
 
 function start () {
     var btnHint;
-    btnHint = document.getElementById ('hint-button-1');
+    btnHint = $('#hint-button-1')[0];
     btnHint.onclick = giveHint1;
-    btnHint = document.getElementById ('hint-button-2');
+    btnHint = $('#hint-button-2')[0];
     btnHint.onclick = giveHint2;
     
-    result = document.getElementById ('result-area');
-    editor = document.getElementById ('editor-area');
+    result = $('#result-area')[0];
+    editor = $('#editor-area')[0];
     editor.onkeyup = applyCode;
     editor.onpaste = applyCode;
 }
